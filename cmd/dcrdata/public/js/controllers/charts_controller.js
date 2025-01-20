4e0d5ac0871aa4757b991d7ec2eab80fcc36b9c4
thanhnp95 <thanhnp.eco@gmail.com>
2025-01-20 09:26:28 -0600

    Adjust the y-axis label of the hashrate and chainwork charts

--- cmd/dcrdata/public/js/controllers/charts_controller.js
+++ cmd/dcrdata/public/js/controllers/charts_controller.js
@@ -632,14 +632,14 @@ export default class extends Controller {
 
       case 'chainwork': // Total chainwork over time
         d = zip2D(data, data.work)
-        assign(gOptions, mapDygraphOptions(d, [xlabel, 'Cumulative Chainwork'],
+        assign(gOptions, mapDygraphOptions(d, [xlabel, 'Cumulative Chainwork (exahash)'],
           false, 'Cumulative Chainwork', true, false))
         yFormatter = customYFormatter(y => withBigUnits(y, chainworkUnits))
         break
 
       case 'hashrate': // Total chainwork over time
         d = zip2D(data, data.rate, 1e-3, data.offset)
-        assign(gOptions, mapDygraphOptions(d, [xlabel, 'Network Hashrate'],
+        assign(gOptions, mapDygraphOptions(d, [xlabel, 'Network Hashrat (petahash/s)'],
           false, 'Network Hashrate', true, false))
         yFormatter = customYFormatter(y => withBigUnits(y * 1e3, hashrateUnits))
         break
